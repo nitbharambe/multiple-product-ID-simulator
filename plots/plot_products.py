2450fbf9b9c5a6ab5887657120aa1e5509f6236b
Nitish Bharambe <78108900+nitbharambe@users.noreply.github.com>
2021-11-09 18:58:46 +0100

    Fixed xaxis of product plot 2

--- plots/plot_products.py
+++ plots/plot_products.py
@@ -2,11 +2,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import seaborn as sns
-import matplotlib.dates as mdates
-import matplotlib.ticker as ticker
 
 def product_plot(exp_path, NordPool_all):
-    sel_plot = '_cum_positions'
+    #sel_plot = '_cum_positions'
+    sel_plot = 'all_positions'
     data_trader, data_p_no, data_time, data_volume = [], [], [], []
     # Position plot method selection: From all_positions or _cum_positions of traders
     if sel_plot == '_cum_positions':
@@ -31,14 +30,17 @@ def product_plot(exp_path, NordPool_all):
     data_heatmap.replace(np.nan, 0)
     data_heatmap['p_no'] = data_heatmap['p_no'] + 1
     data_heatmap['time'] = pd.to_datetime(data_heatmap['time'])
+    # Saving DF for quicker plots
+    #data_heatmap.to_pickle("C:/Users/nitbh/OneDrive/Documents/repository/intraday-market-simulator-master/results/heatmapdata/data_heatmap.pkl")
 
     for id, items in NordPool_all.products[0].traders.items():
         pivoted = data_heatmap[data_heatmap['trader'] == id].pivot(index="p_no", columns="time", values="volume")
-        plt.figure()
-        snsax = sns.heatmap(pivoted, center=0, robust=True)
+        plt.figure(figsize=(15,8))
+        snsax = sns.heatmap(pivoted, center=0)
         snsax.set_title("%s Traded Volume" % (id))
         snsax.set_ylabel("Product Number")
         snsax.set_xlabel("Time")
         snsax.set_xticklabels(pivoted.columns.strftime('%H:%M'))
         plt.savefig("%s/a_%s_%s%s" % (exp_path, "heatmap", id, ".png"), dpi=500, bbox_inches='tight')
+        #plt.show()
 
